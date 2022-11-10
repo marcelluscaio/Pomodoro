6d22130ac46771e3daab6a5f02a19ec4960d80e7
Caio Cabral <ocaiocabral@gmail.com>
2022-11-10 10:41:38 -0400

    Countodwon creation

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ Pomodoro
 
 
 Tarefas:
-- [x] Create timeout
-- [x] Create Pomodoro activator
-- [ ] Conect through JS Pomodor activator
+- [x] Create timeout;
+- [x] Create Pomodoro activator;
+- [X] Conect through Pomodoro activator through JS;
+- [x] Create total time controler;
+- [x] Create countdown per second;
 - [ ] Control time, and after 25 minutes let user know time is out
 - [ ] Another tab for 5 min pause
 - [ ] Show countdown
--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 const startButton = document.querySelector("#start");
+let totalTime = 4000
+let timeLeft = totalTime/1000
 
-startButton.addEventListener("click", timer)
-   
-
-let minutos = 0;
-let segundos = 3;
-
-let milisegundosPomodoro = 1000*segundos;
+startButton.addEventListener("click", timer);
 
 function timer(){
-   setTimeout(() => {
-      console.log("Pomodoro!");   
-   }, "3000")
-}
+   console.log("Começou Pomodoro!"); 
+   counter = setInterval(() => {      
+      timeLeft--
+      console.log(timeLeft);   
+   }, "1000");
+   setTimeout(() => {      
+      console.log("Acabou o Pomodoro!");
+      clearInterval(counter);
+   }, totalTime);
+};
+
+/* let minutos = 0;
+let segundos = 3;
+let milisegundosPomodoro = 1000*segundos; */
